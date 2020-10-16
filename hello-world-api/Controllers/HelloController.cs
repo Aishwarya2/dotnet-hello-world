0567d93b8692bdb755c01dfedd06c9df5ed5642b
Aishwarya Sundararajan <aishsun@amazon.com>
2020-10-16 12:00:49 -0700

    Introducing antipattern

--- hello-world-api/Controllers/HelloController.cs
+++ hello-world-api/Controllers/HelloController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
 
 namespace hello_world_api.Controllers
 {
     [Route("api/[controller]")]
     public class HelloController : Controller
     {
+        private RegistryKey registryKey;
+
         // GET api/helllo
         [HttpGet]
         public string Get()
         {
+            registryKey = Registry.CurrentUser.CreateSubKey("test");
+            registryKey.CreateSubKey("test");
             return "Hello world!!!!!!";
         }
 
